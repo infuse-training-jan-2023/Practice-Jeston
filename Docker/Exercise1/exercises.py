700d090801679c2c5fb2e3ca56f082e6a112701c
jeston13 <imjeston@gmail.com>
2023-01-27 02:42:07 +0530

    docker-exercises

---
+++ Docker/Exercise1/exercises.py
@@ -0,0 +1,21 @@
+import sys
+for arg in sys.argv:
+    print(arg)
+l = len(sys.argv[1])
+li = sys.argv[1][1:l-1].split(',')
+
+    # Create an empty list
+filter_arr = []
+
+# go through each element in arr
+for element in li:
+  # if the element is higher than 42, set the value to True, otherwise False:
+  if int(element) > 42:
+    filter_arr.append(True)
+  else:
+    filter_arr.append(False)
+
+# newarr = li[filter_arr]
+
+print(filter_arr)
+# print(newarr)
