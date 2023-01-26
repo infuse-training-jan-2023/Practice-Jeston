ee3113dbbe806112a85c6b93f2f92a0ab407b2cc
jeston13 <imjeston@gmail.com>
2023-01-26 04:14:37 -0800

    add/sub/mul

--- Calculator.rb
+++ Calculator.rb
@@ -8,9 +8,14 @@ class Calculator
         return num1-num2      
     end
 
+    def multiply(num1 , num2)   
+        return num1*num2      
+    end
+
 end
 
 Calculator_instance=Calculator.new
 puts Calculator_instance.add(2,3)
 puts Calculator_instance.subtract(3,2)
+puts Calculator_instance.multiply(2,3)
 
