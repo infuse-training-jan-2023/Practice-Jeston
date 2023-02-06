d7253b3eba1c9123a338b3cddcb63fd9f8cc8d4f
jeston13 <imjeston@gmail.com>
2023-02-06 17:56:59 +0530

    changes done for first PR

--- Docker/Exercise2/Test_ruby.rb
+++ Docker/Exercise2/Test_ruby.rb
@@ -3,6 +3,9 @@ for arg in ARGV
     puts arg
  end
 
+ puts "The command line argument supplied was"
+ puts Numerizer.numerize(ENV["TEST"])
+
  for arg in ARGV
     puts Numerizer.numerize(arg)
  end
