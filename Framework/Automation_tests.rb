532914c5c9c3d31018afbb4f811c5b28d7eb84d5
jeston13 <imjeston@gmail.com>
2023-02-06 18:40:31 +0530

    passed object of driver as parameter

--- Framework/Automation_tests.rb
+++ Framework/Automation_tests.rb
@@ -11,36 +11,37 @@ class MyTests < Test::Unit::TestCase
     @local_url="file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html"
 
 
-    def test_check_if_instance
-        instance_of_tests = Tests.new(Driver.new)
-        assert instance_of_tests.instance_of? Tests
-    end
+    # def test_check_if_instance
+    #     instance_of_tests = Tests.new(Driver.new)
+    #     assert instance_of_tests.instance_of? Tests
+    # end
 
     def test_naviagte_to_url_positive
-        instance=Tests.new(Driver.new)
+        driver_object=Driver.new.get_driver()
+        instance=Tests.new(driver_object)
         assert_equal("Navigated sucessfully",instance.navigate_to_and_maximize("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html"))
     end
 
     def test_naviagte_to_url_negative
-        instance=Tests.new(Driver.new)
+        driver_object=Driver.new.get_driver()
+        instance=Tests.new(driver_object)
         assert_equal("The error invalid argument\n" +
         "  (Session info: chrome=109.0.5414.120) occured",instance.navigate_to_and_maximize("abc"))
     end
 
     def test_get_element_by_id_positive
-        instance=Tests.new(Driver.new)
-        instance.navigate_to_and_maximize("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
-        # name=driver_object. ("text").send_keys("hello")
+        driver_object=Driver.new.get_driver()
+        driver_object.get("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
+        instance=Tests.new(driver_object)
         name=instance.get_element_by_id("text")
-        #name=driver_object.find_element(:id,"text")
-
         name.send_keys("test")
-        assert_equal("test",instance.get_text(name))
+        assert_equal("test",name['value'])
     end
 
     def test_get_element_by_id_negative
-        instance=Tests.new(Driver.new)
-        instance.navigate_to_and_maximize("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
+        driver_object=Driver.new.get_driver()
+        driver_object.get("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
+        instance=Tests.new(driver_object)
         assert_equal("no such element: Unable to locate element: {\"method\":\"css selector\",\"selector\":\"#jeston\"}\n" +
         "  (Session info: chrome=109.0.5414.120)",instance.get_element_by_id('jeston'))
     end
@@ -52,31 +53,36 @@ class MyTests < Test::Unit::TestCase
     # # end
 
     def test_get_element_by_name_negative
-        instance=Tests.new(Driver.new)
-        instance.navigate_to_and_maximize("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
+        driver_object=Driver.new.get_driver()
+        driver_object.get("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
+        instance=Tests.new(driver_object)
         assert_equal([],instance.get_element_by_name("noexist"))
     end
 
-    # # def test_get_element_by_tag_positive
-    # #     instance=Tests.new("/home/jestonfernandes/HTML_Training_INfuse/selenium/chromedriver_linux64/chromedriver")
-    # #     assert_equal("element found",instance.get_element_by_tag("input"))
-    # # end
+    # # # def test_get_element_by_tag_positive
+    # # #     instance=Tests.new("/home/jestonfernandes/HTML_Training_INfuse/selenium/chromedriver_linux64/chromedriver")
+    # # #     assert_equal("element found",instance.get_element_by_tag("input"))
+    # # # end
 
     def test_get_element_by_tag_negative
-        instance=Tests.new(Driver.new)
-        instance.navigate_to_and_maximize("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
+        driver_object=Driver.new.get_driver()
+        driver_object.get("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
+        instance=Tests.new(driver_object)
         assert_equal([],instance.get_element_by_tag("noexist"))
     end
 
     def test_scroll_positive
-        instance=Tests.new(Driver.new)
-        instance.navigate_to_and_maximize("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
+        driver_object=Driver.new.get_driver()
+        driver_object.get("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
+        instance=Tests.new(driver_object)
         assert_equal("scroll successfull",instance.scroll("200"))
     end
 
     def test_scroll_negative
-        instance=Tests.new(Driver.new)
-        instance.navigate_to_and_maximize("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
+        driver_object=Driver.new.get_driver()
+        driver_object.get("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
+        instance=Tests.new(driver_object)
+        #instance.navigate_to_and_maximize("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
         assert_equal("The error javascript error: abc is not defined\n" +
         "  (Session info: chrome=109.0.5414.120) occured",instance.scroll("abc"))
     end
@@ -101,8 +107,9 @@ class MyTests < Test::Unit::TestCase
     end   
 
     def test_page_title
-        instance = Tests.new(Driver.new)
-        instance.navigate_to_and_maximize("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
+        driver_object=Driver.new.get_driver()
+        driver_object.get("file:///C:\\Users\\Jeston\\Desktop\\test\\Practice-Jeston\\Framework\\form.html")
+        instance=Tests.new(driver_object)
         assert_equal("Test form",instance.get_title())
     end
 
