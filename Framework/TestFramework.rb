a55e5acd6b116250f54d2cb92386d7ba8ff1e5fa
jeston13 <52904894+jeston13@users.noreply.github.com>
2023-02-07 11:27:54 +0530

    Update TestFramework.rb

--- Framework/TestFramework.rb
+++ Framework/TestFramework.rb
@@ -4,15 +4,15 @@ class Driver
     attr_accessor :driver
 
     def initialize()
-        # Selenium::WebDriver::Chrome.driver_path = "/opt/chromedriver-109.0.5414.74/chromedriver"
-        driver_path= "C:\\Users\\Jeston\\Downloads\\chromedriver_win32\\chromedriver.exe" 
-        Selenium::WebDriver::Chrome::Service.driver_path=driver_path           
+        Selenium::WebDriver::Chrome.driver_path = "/opt/chromedriver-109.0.5414.74/chromedriver"
+        #driver_path= "C:\\Users\\Jeston\\Downloads\\chromedriver_win32\\chromedriver.exe" 
+        #Selenium::WebDriver::Chrome::Service.driver_path=driver_path           
         options = Selenium::WebDriver::Chrome::Options.new
-        # options.add_argument("--headless")
-        # options.add_argument("--no-sandbox")
-        # options.add_argument("--window-size=1366,768")
-        # options.add_argument("--disable-gpu")
-        # options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--window-size=1366,768")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-dev-shm-usage")
         @driver = Selenium::WebDriver.for :chrome, options: options
     end
 
